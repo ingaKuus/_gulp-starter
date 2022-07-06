454d1f9abf205f58e821efabe2bc59eaf92fb095
ingaKuus <ingakuus13@gmail.com>
2022-07-06 09:36:57 +0300

    pushing public

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+
+
+$(function() {
+  const isMobile = $(window).width() <= 767;
+
+  
+});
+
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIlxyXG5cclxuJChmdW5jdGlvbigpIHtcclxuICBjb25zdCBpc01vYmlsZSA9ICQod2luZG93KS53aWR0aCgpIDw9IDc2NztcclxuXHJcbiAgXHJcbn0pO1xyXG5cclxuIl0sImZpbGUiOiJtYWluLmpzIn0=
